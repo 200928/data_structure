ccc5347d6071ad52decfa5f3dd70caeb1fc62408
minghuifan <755400668@qq.com>
2022-06-18 10:29:57 +0800

    MyBatis更新

---
+++ Mybatis/src/main/java/com/ming/App.java
@@ -0,0 +1,13 @@
+package com.ming;
+
+/**
+ * @author MingHui Fan
+ * @version 1.0
+ * @date 2022/6/18-上午10:08
+ * @description com.ming
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
